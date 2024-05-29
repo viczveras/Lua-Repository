97290185913be2c7d56686aa492501041d4a1b9b
Victor Véras <60857603+viczveras@users.noreply.github.com>
2024-05-29 00:55:42 -0300

    commit teste
    commit teste de arquivos utilizados no estudo

---
+++ VSCODE/main.lua
@@ -0,0 +1,34 @@
+--[[
++ - * / % ^
+]] 
+
+num1=10
+num2=5
+-- soma=num1+num2^2
+--print(20%3)
+
+--[[
+Relacionais > >= < <= ~= ==
+]]
+
+a=20~=20
+--print(a)
+
+--[[
+Lógicos    or and not nil
+and = retorna primeiro se for falso, se não for retornar o segundo.
+or = retorna o primeiro se ele não for falso
+not = inverte.
+]]
+
+--print(10 and 2) --2
+
+
+name,surname=nil,nil
+--print(name, surname)
+
+nota1,nota2=50,30
+local soma=nota1+nota2
+do
+    print(soma)
+end
