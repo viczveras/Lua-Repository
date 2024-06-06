51959d78fb4c6c3b4b302f608dcdc90e1585d71f
Victor Véras <60857603+viczveras@users.noreply.github.com>
2024-06-05 21:57:56 -0300

    Inicio game terminal lua

--- VSCODE/main.lua
+++ VSCODE/main.lua
@@ -1,203 +1,41 @@
---[[while enquanto for verdadeiro, realizará o processo, caso não, caira no end.
-
-num=0
-while num<10 do
-    print(num)
-    num=num+1
+function Open_game()
+    print("Bem vindo ao jogo")
+    print("Desenvolvido por Victor Veras")
 end
 
-print("\n")
+function Manual_game()
+    print("Voce deseja ler o manual do jogo?(S, ou N,)")
+      local resposta_manual=io.read()
+        if resposta_manual=="S" then
+            print[["Este e um jogo de adivinhacao, onde o seu objetivo e adivinhar qual o numero que o computador esta pensando.
+            Voce como jogador devera chutar um numero e diremos se esta correto ou errado.
+            Se estiver errado, daremos dicas para que voce continue tentando."]]
+        end
+    end
 
---repeat : até que seja verdadedeiro.
-num=0
-repeat 
-    print(num)
-    num=num+1
-until num>10
-
---for numérico
-dias={"Domingo","Segunda","Terca","Quarta","Quinta","Sexta","Sabado"}
---for i = 1,7 do
---    print(dias[i])
---   
--- end
-
-for k,v in pairs(dias) do
-    print(k.." - "..v)
-    
-end
---for tradicional
-nome="Victor"
-for v in string.gmatch(nome,".") do
-    print(v)
+function Random_number()
+    return 50
     
 end
 
-i=0
--- o return também poderia ser usado no local mas acabaria com o programa.
-while i<100 do
-    print(i)
-    if i > 10 then 
-        break
-    end
-    i=i+1
+function User_answer()
+    while true do 
+        answer = io.read()
+        if type(tonumber(answer)) == "number" then --se a resposta for do tipo número faça
+        answer = tonumber(answer)
+        if answer > 100 or answer < 1 then
+            print("Digite um numero entre 1 e 100")
+        else 
+            return answer    
+        end
+    else
+        print("Voce deve digitar apenas numeros")
+        end
 end
-print("Fim da execucao")
-
-
--- go to
-
-::ini::
-print("Digite um numero: ")
-numero=io.read()
-print("Numero digitado: "..numero)
-print("\nDigitar outro numero? ")
-resp=io.read()
-
-if resp=="s" then
-    goto ini
-    
 end
 
 
--------
-
---[[
-::sala1:: do
- print("\n\nVoce esta na sala 1")
- print("Deseja ir para qual sala? [1,2,3,4,]")
- local sala=io.read()
- if     sala=="1" then
-    goto sala1
- elseif sala=="2" then
-    goto sala2
-elseif  sala=="3" then
-    goto sala3
-elseif sala=="4" then 
-    goto sala4   
-else 
-    print("Sala invalida")
- end
-
-end
-
-::sala2:: do
-    print("\n\nVoce esta na sala 2")
-    print("Deseja ir para qual sala? [1,2,3,4,]")
-    local sala=io.read()
-    if     sala=="1" then
-       goto sala1
-    elseif sala=="2" then
-       goto sala2
-   elseif  sala=="3" then
-       goto sala3
-   elseif sala=="4" then 
-       goto sala4   
-   else 
-       print("Sala invalida")
-    end
-   
-   end
-   
-   ::sala3:: do
-    print("\n\nVoce esta na sala 3")
-    print("Deseja ir para qual sala? [1,2,3,4,]")
-    local sala=io.read()
-    if     sala=="1" then
-       goto sala1
-    elseif sala=="2" then
-       goto sala2
-   elseif  sala=="3" then
-       goto sala3
-   elseif sala=="4" then 
-       goto sala4   
-   else 
-       print("Sala invalida")
-    end
-   
-   end
- --]]  
-  --[[ ::sala4:: do
-    print("\n\nVoce esta na sala 4, e descobriu a saida! Parabens")
-    print("Fim de jogo")
-    local sala=io.read()
-    if     sala=="1" then
-       goto sala1
-    elseif sala=="2" then
-       goto sala2
-   elseif  sala=="3" then
-       goto sala3
-   elseif sala=="4" then 
-       goto sala4   
-   else 
-       print("Sala invalida")
-    end
-   
-   end
-   
- --]]
-
-   -- listas 
-
-   num={10,5,8,2,3,4,5,7,5,2,9}
-   --for i = 1,10 do
-   -- print(num[i])
-   --end
-
---[[for i=1, #num do
-    print(num[i])
-   end
-print("---------------")
-soma=0
-for i=1, #num do
-    soma=soma+num[i]
-end
-print("Soma "..soma)
---]]
-
---functions
-
---[[function ola()
-print("Seja bem vindo")
-print("Continuação Lua")
-print("----------------")
-end    
-
-ola()
---]]
-print("Digite o primeiro valor: ")
-num1=io.read()
-print("Digite o segundo valor: ")
-num2=io.read()
-
---[[function soma(n1,n2)
-    res=n1+n2
-    print("A soma de "..n1.." e "..n2.." da "..res)
-    print("-------------------------------")
-end   
---]]
--- tá funcional, mas não sei pq o debugger não roda esse código
---[[function calc(n1,n2)
-    soma=n1+n2
-    mult=n1*n2
-    subt=n1-n2
-    divz=n2/n2
-
-    return soma,mult,subt,divz
-end
-rsoma,rmult,rsubt,rdivz=calc(num1,num2)
+print("Hello world")
 
 
-print(rsoma,rmult,rsubt,rdivz)
-print("-------------------------------")
---]]
 
---funcão variadica
-function soma(...)
-    local s=0
-    for i,v in pairs{...} do
-        s=s+v
-    end
- return s
-end
- print("Soma: "..soma(1,2,3,))
